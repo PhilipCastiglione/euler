c0a7b7221dbe605248f1cd81ca17c242f7c25803
Philip Castiglione <philipcastiglione@gmail.com>
2016-08-09 16:04:49 +1000

    pr22 in C#! woo!

---
+++ pr22/Pr22.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Pr22
+{
+    public class Euler
+    {
+        public static void Main(string[] args)
+        {
+            string raw_input = File.ReadAllLines("./p022_names.txt")[0];
+            string[] names = raw_input.Replace("\"","").Split(',');
+            Array.Sort(names);
+            
+            int i = 0;
+            int name_score;
+            int total_score = 0;
+            foreach(string name in names)
+            {
+                name_score = 0;
+
+                foreach(char c in name)
+                {
+                    name_score += (int) c - 64;
+                }
+
+                total_score += name_score * ++i; 
+            }
+
+            Console.WriteLine(total_score);
+        }
+    }
+}
