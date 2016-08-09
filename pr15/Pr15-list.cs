97d53573fba8cb6fc5e357a5aa080bb065a7b0d0
Philip Castiglione <philipcastiglione@gmail.com>
2016-08-10 09:30:22 +1000

    ignore the last commit it was silly, also, here is pr15 in C#

---
+++ pr15/Pr15-list.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApplicationList
+{
+    public class Program
+    {
+        public static List<long> BuildArray(int size)
+        {
+            var built_array = new List<long>();
+            built_array.Add(1);
+
+            if(size != 1)
+            {
+                var smaller_builder = BuildArray(size - 1);
+
+                for(int i = 1; i < smaller_builder.Count; i++)
+                {
+                    built_array.Add(built_array.Last() + smaller_builder[i]);
+                }
+            }
+
+            built_array.Add(built_array.Last() * 2);
+
+            return built_array;
+        }
+        public static void ListMain(string[] args)
+        {
+            var euler_array = BuildArray(Int32.Parse(args[0]));
+
+            Console.WriteLine(euler_array[euler_array.Count - 1]);
+        }
+    }
+}
--- pr15/Pr15.cs
+++ pr15/Pr15.cs
@@ -4,9 +4,9 @@ namespace ConsoleApplication
 {
     public class Program
     {
-        public static int[] BuildArray(int size)
+        public static long[] BuildArray(int size)
         {
-            var built_array = new int[size + 1];
+            var built_array = new long[size + 1];
             built_array[0] = 1;
 
             if(size != 1)
@@ -16,18 +16,10 @@ namespace ConsoleApplication
                 for(int i = 1; i < smaller_builder.Length; i++)
                 {
                     built_array[i] = built_array[i - 1] + smaller_builder[i];
-                    i++;
                 }
             }
 
             built_array[size] = built_array[size - 1] * 2;
-
-            Console.WriteLine("SIZE: {0}", size);
-            Console.WriteLine("ARY LEN: {0}", built_array.Length);
-            for(int i = 0; i < built_array.Length; i++)
-            {
-                Console.WriteLine("ARY IDX {0}: {1}", i, built_array[i]);
-            }
 
             return built_array;
         }
