f81ff084df51b687920913285d70efed3bc2cc7e
Philip Castiglione <philipcastiglione@gmail.com>
2016-08-09 17:34:40 +1000

    amends to pr22 c#

--- pr22/Pr22.cs
+++ pr22/Pr22.cs
@@ -7,18 +7,17 @@ namespace Pr22
     {
         public static void Main(string[] args)
         {
-            string raw_input = File.ReadAllLines("./p022_names.txt")[0];
-            string[] names = raw_input.Replace("\"","").Split(',');
+            var raw_input = File.ReadAllText("./p022_names.txt");
+            var names = raw_input.Replace("\"","").Split(',');
             Array.Sort(names);
             
-            int i = 0;
-            int name_score;
-            int total_score = 0;
-            foreach(string name in names)
+            var i = 0;
+            var total_score = 0;
+            foreach(var name in names)
             {
-                name_score = 0;
+                var name_score = 0;
 
-                foreach(char c in name)
+                foreach(var c in name)
                 {
                     name_score += (int) c - 64;
                 }
